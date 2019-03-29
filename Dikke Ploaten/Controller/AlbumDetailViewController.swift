1562cb10eecf1e4a8078ccae69843fbe225478f0
Victor Vanhove <victorvanhove@hotmail.com>
2019-03-29 16:22:02 +0100

    VDP 13 - Added new JSON data to albumDetailView

--- Dikke Ploaten/Controller/AlbumDetailViewController.swift
+++ Dikke Ploaten/Controller/AlbumDetailViewController.swift
@@ -15,6 +15,12 @@ class AlbumDetailViewController: UIViewController {
 	@IBOutlet weak var lblTitle: UILabel!
 	@IBOutlet weak var lblArtist: UILabel!
 	@IBOutlet weak var imgCover: UIImageView!
+	@IBOutlet weak var txtDescription: UITextView!
+	@IBOutlet weak var lblGenre: UILabel!
+	@IBOutlet weak var lblReleaseYear: UILabel!
+	@IBOutlet weak var txtTracklist: UITextView!
+	@IBOutlet weak var txtMusicians: UITextView!
+	
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -22,6 +28,11 @@ class AlbumDetailViewController: UIViewController {
 		lblTitle.text = album.title
 		lblArtist.text = album.artist
 		imgCover.image = UIImage(data: try! Data(contentsOf: URL(string: album.cover)!))
+		txtDescription.text = album.description ?? "blabla"
+		lblGenre.text = album.genre ?? "Rock"
+		lblReleaseYear.text = album.releaseYear ?? "Juli 1979"
+		txtTracklist.text = album.tracklist?.replacingOccurrences(of: "\\n", with: "\n") ?? ""
+		txtMusicians.text = album.musicians?.replacingOccurrences(of: "\\n", with: "\n") ?? ""
 	}
 	
 }
